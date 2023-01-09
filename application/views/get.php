eb41be8ce5994128fd07b5970bfba6618c9e4e40
221314 <107024244+221314@users.noreply.github.com>
2023-01-09 18:27:38 +0900

    Delete application directory

--- application/views/get.php
+++
@@ -1,6 +0,0 @@
-<article> 
-<h1><?=$topic->title?></h1> <!--제목-->
-<div>
-    <?=$topic->description?> <!--본문-->
-</div>
-</article>
--- application/views/topic_list.php
+++
@@ -1,9 +0,0 @@
-<ul>
-<?php
-foreach($topics as $entry){ //topics를 entry갯수만큼 반복하는 php함수
-    ?>
-    <li><a href="/index.php/Topic/get/<?=$entry->id?>"><?=$entry->title?></a></li>
-    <?php //href를 사용해서 링크를 걸어준다. (URI 구성)=>/컨트롤클래스/호출할함수/전달파라미터
-}
-?>
-</ul>
